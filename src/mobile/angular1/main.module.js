8cdae648817f4c9520fdb480579609ee099f8af4
roliod <rolandoduberu@gmail.com>
2018-05-23 11:11:20 +0100

    indent

--- src/mobile/angular1/main.module.js
+++ src/mobile/angular1/main.module.js
@@ -5,45 +5,45 @@ angular.module('CallApp', ['ionic','ngCordova','CallAppcontrollers', 'ngCordova.
 
   $ionicPlatform.ready(function() {
 
-		var push = PushNotification.init({
-        android: {
-        	senderID: '847073181237',
-        	forceShow: 'true',
-          sound: 'true',
-          vibrate: 'true'
-        },
-        browser: {
-            pushServiceURL: 'http://push.api.phonegap.com/v1/push'
-        },
-        ios: {
-        	alert: "true",
-        	badge: "true",
-        	sound: "true"
-        },
-        windows: {}
-		});
+    var push = PushNotification.init({
+      android: {
+        senderID: '847073181237',
+        forceShow: 'true',
+        sound: 'true',
+        vibrate: 'true'
+      },
+      browser: {
+        pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+      },
+      ios: {
+        alert: "true",
+        badge: "true",
+        sound: "true"
+      },
+      windows: {}
+    });
 
-		push.on('registration', (data) => {
+    push.on('registration', (data) => {
 
-			//at this point you can send the device token to your server, you would need it
-			console.log ('deviceToken:' + data.registrationId);
-		});
+      //at this point you can send the device token to your server, you would need it
+      console.log ('deviceToken:' + data.registrationId);
+    });
 
-		push.on('notification', (data) => {
-			console.log ('Notification:' + data);
-			// data.message,
-			// data.title,
-			// data.count,
-			// data.sound,
-			// data.image,
-			// data.additionalData
-		});
+    push.on('notification', (data) => {
+      console.log ('Notification:' + data);
+      // data.message,
+      // data.title,
+      // data.count,
+      // data.sound,
+      // data.image,
+      // data.additionalData
+    });
 
-		push.on('error', (e) => {
-			 console.log ('Error' + e.message);
-		});
+    push.on('error', (e) => {
+      console.log ('Error' + e.message);
+    });
 
 
-	});
+  });
 
 })
